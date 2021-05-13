f1b1d2e3c5eead6670b6270abb0465e8e51a36d9
Omar-prog1 <82549988+Omar-prog1@users.noreply.github.com>
2021-05-12 21:18:18 -0500

    Add files via upload

---
+++ Ex1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+   {
+    float primer, segundo, tercero;
+    printf("Hernández Flores Ricardo Omar 4IV17");
+    printf("\n\n programa 1");
+    printf("\n\n Saca el promedio de valores reales");
+    printf("\n\n Dame el primer valor real");
+    scanf("%f",&primer);
+    printf("\n\n Dame el segundo valor real");
+    scanf("%f",&segundo);
+    tercero =(primer + segundo)/2;
+    printf("\nEl cociente es %f", tercero);
+     return 0;
+    
+   }
